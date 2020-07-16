0d468ac57024a619941296d5b78c7579e29e0858
macarena chang <changmacarena@gmail.com>
2020-07-16 04:44:16 -0300

    Diagonales parte 1

--- ChallengeMutantes/src/App.java
+++ ChallengeMutantes/src/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -5,29 +6,33 @@ public class App {
     public static boolean isMutant(String[] dna) {
 
         int contadorFinal = 0;
-       
-            //check for horizontal matches.
-            int contadorHorizontal = checkMat(getMatrix(dna));
-            contadorFinal = contadorHorizontal; 
-            if(contadorFinal>3){
-                return true; 
-            }
-
-            //check for vertical matches.
-            int contadorVertical =  checkMat(getMatrixTransposed(getMatrix(dna)));
-            contadorFinal = contadorFinal + contadorVertical;
-            if(contadorFinal>3){
-                return true; 
-            }
-
-           
         
+        
+
+        // check for horizontal matches.
+        int contadorHorizontal = checkMat(getMatrix(dna));
+        contadorFinal = contadorHorizontal;
+        if (contadorFinal > 3) {
+            return true;
+        }
+
+        // check for vertical matches.
+        int contadorVertical = checkMat(getMatrixTransposed(getMatrix(dna)));
+        contadorFinal = contadorFinal + contadorVertical;
+        if (contadorFinal > 3) {
+            return true;
+        }
+
+        //Check for diagonal matches
+        int contadorDiag2 = diagonals2(getMatrix(dna));
+        contadorFinal = contadorFinal + contadorDiag2;
+        if (contadorFinal > 3) {
+            return true;
+        }
+
         System.out.println("Contador final: " + contadorFinal);
 
-        
-       
-            return false;
-
+        return false;
 
     }
 
@@ -51,42 +56,100 @@ public class App {
 
     public static char[][] getMatrixTransposed(char[][] mat) {
 
-        char[][] transpose = new char[mat.length][mat.length];
+        char[][] transposed = new char[mat.length][mat.length];
 
         for (int i = 0; i < mat.length; i++) {
 
             for (int j = 0; j < mat.length; j++) {
 
-                transpose[i][j] = mat[j][i];
+                transposed[i][j] = mat[j][i];
 
             }
         }
-        System.out.println(transpose);
-        return transpose; 
+        return transposed;
 
     }
 
-    public static int checkMat(char[][] mat){
+    public static int diagonals2(char[][] mat) {
+        int contador=0; 
+        char[][] matrix = mat;
+
+        // anti diag superiores
+        //K=3 porque el pattern tiene al menos 4 letras por lo tanto la diagonal debe ser como minimo de 4 caracteres.
+        for (int k = 3; k <= matrix.length - 1; k++) {
+            List diag = new ArrayList<>();
+            int i = k;
+            int j = 0;
+
+            while ((i >= 0 && j <= matrix.length - 1)) {
+
+                System.out.println(matrix[i][j]);
+                diag.add(matrix[i][j]);
+                i = i - 1;
+                j = j + 1;
+            }
+           
+            checkPattern(diag);
+            if(checkPattern(diag)){
+                contador++;
+            } 
+            System.out.println("Diagonal: " + diag);
+            // empty list
+            diag.removeAll(diag);
+            System.out.println("Proxima diagonal...");
+
+        }
+
+        // anti diag inferiores 
+        //length - 3 porque minimo necesitamos una diag de 4 caracteres
+        for (int k = 1; k < matrix.length - 3; k++) {
+
+            List diag = new ArrayList<>();
+            int i = matrix.length - 1;
+            int j = k;
+
+            while (j <= matrix.length - 1 && i >= 0) {
+
+                // System.out.println(matrix[i][j]);
+                diag.add(matrix[i][j]);
+                i = i - 1;
+                j = j + 1;
+
+            }
+
+            checkPattern(diag);
+            if(checkPattern(diag)){
+                contador++;
+            }     
+            System.out.println("Diagonal: " + diag);
+            // empty list
+            diag.removeAll(diag);
+            System.out.println("Proxima diagonal...");
+
+        }
+
+        return contador; 
+    }
+
+    public static int checkMat(char[][] mat) {
         int contadorFinal = 0;
 
-        for(int i = 0; i< mat.length ;i++){
+        for (int i = 0; i < mat.length; i++) {
             checkPattern(mat[i]);
 
-            if(checkPattern(mat[i])){
+            if (checkPattern(mat[i])) {
                 contadorFinal++;
             }
         }
-       System.out.println(contadorFinal);
         return contadorFinal;
 
     }
-    public static boolean checkPattern(char[] charArray) {        
+
+    public static boolean checkPattern(char[] charArray) {
 
         if (charArray.length > 3) {
 
             for (int i = 0; i < charArray.length - 3; i++) {
-
-               
 
                 if (charArray[i] == charArray[i + 1] && charArray[i + 1] == charArray[i + 2]
                         && charArray[i + 2] == charArray[i + 3]) {
@@ -96,19 +159,35 @@ public class App {
                 }
 
             }
-            return false; 
+            return false;
         }
 
         return false;
     }
 
-    public static Scanner Teclado = new Scanner(System.in);
+    public static boolean checkPattern(List list) {
+
+        if (list.size()> 3) {
+
+            for (int i = 0; i < list.size() - 3; i++) {
+
+                if (list.get(i)== list.get(i+1) && list.get(i+1) == list.get(i+2)
+                        && list.get(i+2) == list.get(i+3)) {
+
+                    return true;
+
+                }
+
+            }
+            return false;
+        }
+
+        return false;
+    }
 
     public static void main(String[] args) throws Exception {
-        String[] dna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };        
+        String[] dna = { "ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG" };
         isMutant(dna);
-      
 
-      
     }
 }
